5e6a0c6b00ae213e9aabf67f4252449a5d2046dd
RobViego <berlinmoskwa22@gmail.com>
2022-03-27 14:45:52 +0200

    Initial Commit

---
+++ hello.py
@@ -0,0 +1,10 @@
+import os
+os.system("clear")
+
+people_and_their_last_names = {
+	"Bartek": "Mincewicz",
+	"Robert": "Lewandowski"
+}
+
+# prints the text message "hello"
+print(people_and_their_last_names["Robert"])
