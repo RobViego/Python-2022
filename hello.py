19f6081a8de6884242666c574220defff308fae6
RobViego <berlinmoskwa22@gmail.com>
2022-03-27 21:08:17 +0200

    Fizz Buzz with counter

--- hello.py
+++ hello.py
@@ -1,10 +1,29 @@
 import os
 os.system("clear")
 
-people_and_their_last_names = {
-	"Andrew": "Smith",
-	"Robert": "Lewandowski"
-}
+fizz_count = 0
+buzz_count = 0
+fizzbuzz_count = 0
 
-# prints the text message "hello"
-print(people_and_their_last_names["Robert"])
+for i in range(1, 10000000 + 1):
+
+	if i%3 == 0 and i%5 == 0:
+		print(str(i) + " FizzBuzz")
+		fizzbuzz_count += 1
+
+	elif i%3 == 0:
+		print(str(i) + " Fizz")
+		fizz_count += 1
+
+	elif i%5 == 0:
+		print(str(i) + " Buzz")
+		buzz_count += 1
+
+	else:
+		print(i)
+
+print("__________________________")
+print("Fizz\tBuzz\tFizzBuzz")
+print(str("{:,}".format(fizz_count)) + "\t" + str("{:,}".format(buzz_count)) + "\t" + str("{:,}".format(fizzbuzz_count)))
+
+
